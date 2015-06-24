ab7a76b8994dd3c161367b76f4c5927ad0db0928
prieliot <eliot.hautefeuille@gmail.com>
2015-06-24 10:19:47 +0200

    test 2

--- src/core/ADocument.java
+++ src/core/ADocument.java
@@ -1,5 +1,29 @@
 package core;
 
 public abstract class ADocument implements Document {
+	
+	@Override
+	public int number() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void book(Subscriber s) throws UnavailableException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void borrow(Subscriber s) throws UnavailableException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void giveBack() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
