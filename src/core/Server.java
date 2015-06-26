ebabb6400104d8149208e41048ce11aa1638b987
cyril <cyril@Cyril-Pc>
2015-06-26 12:26:32 +0200

    Modif sur Server et Transmission
    Johann suce moi :)

--- src/core/Server.java
+++ src/core/Server.java
@@ -1,8 +1,16 @@
 package core;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server implements Runnable {
+	
+	private ServerSocket servSocket;
+	private Class<? extends Service> serviceType;
+	private Store myStore;
+	
 	public Server(ServerSocket servSocket, Class<? extends Service> serviceType, Store myStore) {
 		this.servSocket = servSocket;
 		this.serviceType = serviceType;
@@ -14,10 +22,27 @@ public class Server implements Runnable {
 		
 		while(true)
 		{
-			//new Thread(serviceType.newInstance()).start();
+				Socket clientSocket;
+				try {
+					
+					clientSocket = servSocket.accept();
+					Transmission t = new Transmission(clientSocket);
+					serviceType.getConstructors()[0].newInstance(myStore,t);	
+					
+				} catch (IOException e) {					
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {		
+					e.printStackTrace();
+				} catch (SecurityException e) {					
+					e.printStackTrace();
+				} catch (InstantiationException e) {					
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {					
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {					
+					e.printStackTrace();
+				}			
 		}		
 	}
-	private ServerSocket servSocket;
-	private Class<? extends Service> serviceType;
-	private Store myStore;
+
 }
--- src/core/Transmission.java
+++ src/core/Transmission.java
@@ -1,5 +1,39 @@
 package core;
 
-public class Transmission {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class Transmission {
+	private Socket clientSocket;
+	private BufferedReader in;
+	private PrintWriter out;
+	
+	public Transmission(Socket clientSocket) throws IOException
+	{	
+		this.clientSocket = clientSocket;
+		this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		this.out = new PrintWriter(clientSocket.getOutputStream(),true);
+	}	
+	public void sendToClient(String s)
+	{
+		out.println(s);
+	}
+	public String recvFromClient() throws IOException
+	{
+		return in.readLine();
+	}
+	public void closeConnection() throws IOException
+	{
+		out.println("Connection closed from server");
+		clientSocket.close();
+		in.close();
+		out.close();
+	}
+	public boolean clientStillAlive()
+	{
+		return clientSocket.isConnected();
+	}
 }
