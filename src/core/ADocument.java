6012df1e509ae1bb04d10b522133729cdc216021
cyril <cyril@Cyril-Pc>
2015-06-27 17:24:15 +0200

    Ajout du GiveBackTimerTask + implémentation de timers dans ADocument

--- src/core/ADocument.java
+++ src/core/ADocument.java
@@ -1,17 +1,23 @@
 package core;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public abstract class ADocument implements Document {
 	
 	private int number;
 	private boolean booked;
 	private boolean available;
 	private Subscriber subscriber;
+	private Timer timer;
+	private TimerTask giveBackTimerTask;
 	
 	public ADocument(int number) {
 		this.number = number;
 		this.available = true;
 		this.booked = false;
 		this.subscriber = null;
+		this.timer = new Timer();
 	}
 	
 	private void checkAvailability() throws UnavailableException { 
@@ -31,26 +37,39 @@ public abstract class ADocument implements Document {
 	@Override
 	public int number() { return number; }
 
+	public void startGiveBackTimer(int time)
+	{
+		this.giveBackTimerTask = new GiveBackTimerTask(this);
+		this.timer.schedule(this.giveBackTimerTask, time);	
+	}
+	
+	public void stopGiveBackTimer()
+	{
+		this.giveBackTimerTask.cancel();
+	}
+	
 	@Override
 	public void book(Subscriber s) throws UnavailableException{
 		checkAvailability();
 		checkBooking(s);
 		this.subscriber = s;
 		this.booked = true;
-		// handle timer
+		startGiveBackTimer(6000);			
 	}
 
 	@Override
 	public void borrow(Subscriber s) throws UnavailableException{
 		checkAvailability();
 		checkBooking(s);
+		stopGiveBackTimer();
 		this.subscriber = s;
-		this.available = true;
-		// handle timer
+		this.available = true;		
+		startGiveBackTimer(60000);
 	}
 
 	@Override
 	public void giveBack(Subscriber s) {
+		
 		this.available = true;
 		this.booked = false;
 		this.subscriber = null;
