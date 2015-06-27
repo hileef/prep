79eed8a39b379b60d7c1f2b8c6113cb50d6c843e
cyril <cyril@Cyril-Pc>
2015-06-27 15:24:00 +0200

    Modif sur ADocument

--- src/core/ADocument.java
+++ src/core/ADocument.java
@@ -3,37 +3,57 @@ package core;
 public abstract class ADocument implements Document {
 	
 	private int number;
+	private boolean booked;
 	private boolean available;
+	private Subscriber subscriber;
 	
 	public ADocument(int number) {
 		this.number = number;
 		this.available = true;
+		this.booked = false;
+		this.subscriber = null;
 	}
 	
 	private void checkAvailability() throws UnavailableException { 
-		if(!available) throw new UnavailableException("Document is currently unavailable");
+		if(!available) throw new UnavailableException("Document unavaible");
+	}
+	
+	private void checkBooking(Subscriber s) throws UnavailableException{ 
+		if(booked)
+		{
+			if(subscriber != s)
+			{
+				throw new UnavailableException("Document already booked");
+			}
+		}
 	}
 	
 	@Override
 	public int number() { return number; }
 
 	@Override
-	public void book(Subscriber s) throws UnavailableException {
+	public void book(Subscriber s) throws UnavailableException{
 		checkAvailability();
-		this.available = false;
+		checkBooking(s);
+		this.subscriber = s;
+		this.booked = true;
 		// handle timer
 	}
 
 	@Override
-	public void borrow(Subscriber s) throws UnavailableException {
+	public void borrow(Subscriber s) throws UnavailableException{
 		checkAvailability();
-		this.available = false;
+		checkBooking(s);
+		this.subscriber = s;
+		this.available = true;
 		// handle timer
 	}
 
 	@Override
 	public void giveBack(Subscriber s) {
 		this.available = true;
+		this.booked = false;
+		this.subscriber = null;
 	}
 
 }
