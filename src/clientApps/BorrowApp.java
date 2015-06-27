8c5213691bf50f5a530c0d8f9920d6b8fd924e88
prieliot <eliot.hautefeuille@gmail.com>
2015-06-27 18:37:20 +0200

    MAJ ports

--- src/clientApps/BorrowApp.java
+++ src/clientApps/BorrowApp.java
@@ -6,6 +6,6 @@ public class BorrowApp extends ClientApp {
 
 	@Override
 	public int port() { return 2600 ; }
-	public static void main(String[] args) throws IOException { (new BookApp()).work(); }
+	public static void main(String[] args) throws IOException { (new BorrowApp()).work(); }
 
 }
